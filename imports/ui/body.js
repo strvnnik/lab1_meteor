d1f13ba205b5756199178e9f075287b8b7fbb755
strvnnik <igor_raider@mail.ru>
2016-12-11 18:22:57 +0300

    Adding user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,6 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
- 
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
  
 import './task.js';
@@ -36,6 +37,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
  
     // Clear form
